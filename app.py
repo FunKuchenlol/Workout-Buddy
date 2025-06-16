c0be34e544b7c46182f950309314c3b91aa0e005
Sarah Lubinus <sarah@codux.tech>
2025-06-16 09:47:03 +0200

    Added all needed html files for device and finished add_device.html and about.html. The Text in about.html.

--- app.py
+++ app.py
@@ -8,20 +8,24 @@ app = FastAPI()
 app.mount('/static', StaticFiles(directory='static'), name='static')
 templates = Jinja2Templates(directory='templates')
 
-@app.get('/')
+# ---------------------- INDEX/HOME ---------------------- #
+
+@app.get('/', name='index')
 def index(request: Request):
-    device_list = read_all_devices()
     return templates.TemplateResponse(
-		'index.html', {'request': request, 'devices': device_list}
+		'index.html', {'request': request}
 	)
 
-@app.get('/add-device')
+
+# ---------------------- ADD DEVICE ---------------------- #
+
+@app.get('/add_device')
 def add_device(request: Request):
     return templates.TemplateResponse(
 		'add_device.html', {'request': request}
 	)
 
-@app.post('/add-device')
+@app.post('/add_device')
 def add_new_device(
     name: str = Form(...),
     active_time: int = Form(...),
@@ -33,4 +37,37 @@ def add_new_device(
     return RedirectResponse(url="/", status_code=303)
 
 
-    
+# ---------------------- DELETE DEVICE ---------------------- #
+@app.get('/delete_device')
+def delete_device(request: Request):
+    return templates.TemplateResponse(
+		'delete_device.html', {'request': request}
+	)
+
+
+# ---------------------- EDIT DEVICE ---------------------- #
+@app.get('/edit_device')
+def edit_device(request: Request):
+    return templates.TemplateResponse(
+		'edit_device.html', {'request': request}
+	)
+
+
+# ---------------------- ALL DEVICES ---------------------- #
+
+@app.get('/all_devices')
+def all_devices(request: Request):
+    device_list = read_all_devices()
+    return templates.TemplateResponse(
+		'all_devices.html', {'request': request, 'devices': device_list}
+	)
+
+
+# ---------------------- ABOUT ---------------------- #
+
+@app.get('/about')
+def about(request: Request):
+    return templates.TemplateResponse(
+		'about.html', {'request': request}
+	)
+
--- static/javascript/theme_changer.js
+++ static/javascript/theme_changer.js
@@ -2,29 +2,28 @@ document.addEventListener("DOMContentLoaded", () => {
     const toggleBtn = document.getElementById("theme-toggle");
     const htmlEl = document.documentElement;
 
-    let currentTheme = ''
-    const themeFromStorage = localStorage.getItem("theme")
+    let currentTheme = '';
+    const themeFromStorage = localStorage.getItem("theme");
 
     if (!themeFromStorage) {
         const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
         currentTheme = prefersDark ? "dark" : "light";
         localStorage.setItem("theme", currentTheme);
-    }
-    else{
-        currentTheme = themeFromStorage
+    } else {
+        currentTheme = themeFromStorage;
     }
 
     htmlEl.setAttribute("data-bs-theme", currentTheme);
-    updateButtonText(currentTheme);
+    updateButtonIcon(currentTheme);
 
     toggleBtn.addEventListener("click", () => {
         currentTheme = (currentTheme === "light") ? "dark" : "light";
         htmlEl.setAttribute("data-bs-theme", currentTheme);
         localStorage.setItem("theme", currentTheme);
-        updateButtonText(currentTheme);
+        updateButtonIcon(currentTheme);
     });
 
-    function updateButtonText(theme) {
-        toggleBtn.innerText = (theme === "light") ? "🌙 Dark Mode" : "☀️ Light Mode";
+    function updateButtonIcon(theme) {
+        toggleBtn.innerText = (theme === "light") ? "🌙" : "☀️";
     }
 });
